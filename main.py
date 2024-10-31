14c9e1b0dac94fd8dffe12f4b82398eb15d72b4a
Mehdi <mohammadmahdi670@gmail.com>
2024-10-31 23:43:07 +0330

    main

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os
 import logging
 import referral as rs
 import course
+from message_handle import handle_message
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s',level=logging.INFO)
@@ -348,28 +349,6 @@ async def callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-
-
-
-# مدیریت پیام‌های ورودی
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    text = update.message.text
-    if text == "معرفی خدمات":
-        await show_welcome(update, context)
-    elif text == "🎓 آموزش و کلاس‌های آنلاین":
-        await course.courses_menu(update, context)
-    elif text == "🌟 خدمات VIP":
-        await show_vip_services(update, context)
-    elif text == "🛠ابزارها":
-        await show_tools(update, context)
-    elif text == "💰 ولت‌های با Win Rate بالا":
-        await show_wallets(update, context)
-    elif text == "🏆 امتیازدهی توییتر":
-        await show_twitter_rating(update, context)
-    elif text == "📣 دعوت دوستان":
-        await show_invite_friends(update, context)
-    elif text == "💼 مشاهده امتیاز":
-        await show_user_score(update,context)
 
 
 
