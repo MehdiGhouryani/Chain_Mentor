3a0e1f36aef7bec8eef584ede40ebc391269ffec
Mehdi <mohammadmahdi670@gmail.com>
2024-11-01 22:58:53 +0330

    wallet

--- course.py
+++ course.py
@@ -130,11 +130,9 @@ async def ask_users_for_feedback(update: Update, context: ContextTypes.DEFAULT_T
 
 async def get_user_info_package(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-
-    # دیباگ: چاپ کردن user_data
     print("Current user_data:", context.user_data)
 
-    if 'package' not in context.user_data:
+    if not context.user_data.get('package'):
         print("-- package initialized --")
         context.user_data['package'] = "GET_NAME"
         await context.bot.send_message(chat_id=chat_id, text="لطفاً نام خود را وارد کنید:")
@@ -142,11 +140,9 @@ async def get_user_info_package(update: Update, context: ContextTypes.DEFAULT_TY
 
 async def get_user_info_online(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-
-    # دیباگ: چاپ کردن user_data
     print("Current user_data:", context.user_data)
 
-    if 'online' not in context.user_data:
+    if not context.user_data.get('online'):
         print("-- online initialized --")
         context.user_data['online'] = "GET_NAME"
         await context.bot.send_message(chat_id=chat_id, text="لطفاً نام خود را وارد کنید:")
