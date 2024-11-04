d07fe29be5e0a8e29fbfa34292c16982b4acea38
Mehdi <mohammadmahdi670@gmail.com>
2024-11-04 12:44:11 +0330

    payment

--- admin_panel.py
+++ admin_panel.py
@@ -14,12 +14,12 @@ cursor = conn.cursor()
 
 
 async def list_courses(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    cursor.execute("SELECT name, description, price FROM courses")
+    cursor.execute("SELECT cource_id,course_name,description,price,course_type,registrants_count,created_at FROM courses")
     courses = cursor.fetchall()
     if courses:
         response = "دوره‌های موجود:\n\n"
         for course in courses:
-            response += f"نام دوره: {course[0]}\nتوضیحات: {course[1]}\nقیمت: {course[2]} تومان\n\n"
+            response += f"ID :{course[0]}\nنام دوره: {course[1]}\nتوضیحات: {course[2]}\nقیمت: {course[3]} تومان\n نوع : {course[4]}\n تعداد افراد عضو شده:  {course[5]}\n تاریخ ایجاد : {course[6]}\n"
     else:
         response = "هیچ دوره‌ای موجود نیست."
     await update.message.reply_text(response)
